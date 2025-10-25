dd3179fbcceb8adc05f6fd399a0a7dba50c4341f
godfreygrace1-collab <godfreygrace1@gmail.com>
2025-10-25 11:48:41 -0600

    initial upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const greeting = document.getElementById ("greeting");
+greeting.style.textAlgin = "center";
+greeting.style.marginTop = "50px";
+const username = window.prompt ("What is your name?", "Grace");
+gretting.textConent = "Welcome to my website, " + username + "!";
